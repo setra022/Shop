efe38ef5c5d37a021022c5700e9019930266a61c
Setra Rakotovao <setra.rakotovao@student-cs.fr>
2023-05-19 22:17:03 +0200

    Ajout de liens entre les pages

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
@@ -10,3 +11,6 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("product", kwargs={"slug": self.slug})
