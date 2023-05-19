784579e0675a7beb8a724d95902c298852fe44f7
Setra Rakotovao <setra.rakotovao@student-cs.fr>
2023-05-19 19:42:41 +0200

    Création du modèle Product

--- store/models.py
+++ store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
